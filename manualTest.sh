ffa420d4471678a5abb9f527d95d7ba673d7c3ef
Robert Pavel <rspavel@lanl.gov>
2019-09-12 09:55:08 -0600

    Improved test script

--- manualTest.sh
+++ manualTest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python ../initTables.py --db testDB.db
-../glueCode_sniffTest  >> cOut.txt &
-python ../alInterface.py --db testDB.db -t DUMMY_TAG_42 -m 3 >> pyOut.txt &
+python $(dirname $BASH_SOURCE)/initTables.py --db testDB.db
+python $(dirname $BASH_SOURCE)/alInterface.py --db testDB.db -t DUMMY_TAG_42 -m 3 &
+$(dirname $BASH_SOURCE)/glueCode_sniffTest
