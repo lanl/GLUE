81f1f21fe4a97b9ba99e5fb332f2bbfa8a86f8bc
Robert Pavel <rspavel@lanl.gov>
2019-10-02 15:56:17 -0600

    Adding Test Script from Verification Run
    Updated bgk test script to version used for verification of coupling run

--- slurmScripts/bgkTest.sh
+++ slurmScripts/bgkTest.sh
@@ -7,19 +7,19 @@ export BGK_DIR=${PATH_TO_BGK_CHECKOUT}
 export LAMMPS_DIR=${PATH_TO_LAMMPS_INSTALL}
 
 # Ensure required directories exist and input deck is available
-export BGK_INP=H-C_test_HHM_TR
+export BGK_INP="H-C_test_HHM_TR 0 3"
 
 # Control variables
 export ALDR_MAX_CONCURRENT_JOBS=4
 
 # Get any required modules
-source ${GLUE_DIR}/slurmScripts/jobEnv.sh
+source ${GLUE_DIR}/slurmScripts/darwin-gnu.sh
 
 # Set up database
-python ${GLUE_DIR}/initTables.py
+python ${GLUE_DIR}/initTables.py -d dummy.db
 
 # Start up Glue Code As A Service
-python ${GLUE_DIR}/alInterface.py -l ${LAMMPS_DIR}/bin/lmp -u $USER -j ${ALDR_MAX_CONCURRENT_JOBS} -d testDB.db -t dummy &
+python ${GLUE_DIR}/alInterface.py -l ${LAMMPS_DIR}/bin/lmp -u $USER -j ${ALDR_MAX_CONCURRENT_JOBS} -d dummy.db -t dummy &
 
 # Call BGK
 srun -n 1 ${BGK_DIR}/exec/MultiBGK_AL_ ${BGK_INP}
