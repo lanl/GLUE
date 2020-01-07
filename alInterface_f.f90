a76fdf38ad842b3dca95598d34c2bc1ec915b7dc
Robert Pavel <rspavel@lanl.gov>
2020-01-07 13:28:56 -0700

    Initial Work on Shale Demo Readiness

--- alInterface_f.f90
+++ alInterface_f.f90
@@ -12,16 +12,29 @@ module alinterface_f
 		real(c_double) :: adsorption
 	end type lbmZeroD_result_f
 
+	type, bind(c) :: lbmDemo_request_f
+		real(c_double) :: density
+		integer(c_int) :: channelWidth
+	end type lbmDemo_request_f
+
+	type, bind(c) :: lbmDemo_result_f
+		real(c_double) :: adsorption
+	end type lbmDemo_result_f
+
 	enum, bind(c)
 		enumerator :: LAMMPS = 0
 		enumerator :: MYSTIC = 1
 		enumerator :: ACTIVELEARNER = 2
 		enumerator :: FAKE = 3
 		enumerator :: DEFAULT = 4
+		enumerator :: FASTLAMMPS = 5
 		enumerator :: KILL = 9
 	end enum
 
 contains
 
+! TODO: Need to add handles to probably pass a c_ptr to sqlitedb
+!   Idea being that we'll handle that on the C side to minimize fortran debugging
+!     and maximize dryness
 
 end module alinterface_f
