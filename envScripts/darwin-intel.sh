729740771185f0949a51fcd09ea45d80331b4e78
Robert Stephen Pavel <rspavel@lanl.gov>
2022-06-20 09:00:50 -0600

    Switching to Docker Based CI

--- envScripts/darwin-intel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-module load gcc/7.4.0 
-module load intel/20.0.1
-module load anaconda/Anaconda3
-module load openmpi/3.1.6-intel_20.0.1
-module load cmake/3.15.0
--- envScripts/jobEnv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-module load gcc/8.2.0
-module load anaconda/Anaconda3
-module load openmpi
